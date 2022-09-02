77d532ababf6a820137bb4a2791a43d595c4694e
Paimon Goulart <paimongoulart@gmail.com>
2022-09-02 16:46:08 -0700

    Add files via upload

--- VCFUnitTester/VcfSimulator.py
+++ VCFUnitTester/VcfSimulator.py
@@ -4,42 +4,6 @@ import sys
 import os
 from IPython.display import SVG, display
 
-ts = msprime.sim_ancestry(3, random_seed=1, sequence_length=10000)
-
-#sequence length and site size have to be set to SAME number
-
-site_size = 10000 #sets number of sites
-
-rlg = np.random.default_rng(1234) #random letter generator 
-
-rchar = rlg.integers(low=0, high=4) #randomly choses number from 0 to 3 which represents genome
-
-difference_counter = range(site_size) 
-
-tables = ts.dump_tables() 
-
-for x in difference_counter: #Randomly assings reference genome for each site
-    
-    rchar = rlg.integers(low=0, high=4)
-        
-    if rchar == 0:
-        letter = "A"
-        
-    elif rchar == 1:
-        letter = "C"
-        
-    elif rchar == 2:
-        letter = "G"
-        
-    elif rchar == 3:
-        letter = "T"
-        
-    tables.sites.add_row(x, letter) 
-    
-ts = tables.tree_sequence()
-
-ts = msprime.sim_mutations(ts, rate=0.1, random_seed=1234) #Mutates the sites at random
-
 def make_site_mask(size, percentmissing = 0, seed=0):
     
     temp_percent = percentmissing/100 #takes percent of user input and converts it to actual percentage
@@ -74,6 +38,7 @@ def make_site_mask(size, percentmissing = 0, seed=0):
     
     return temp_array
 
+
 def make_missing_vcf(ts, percentmissing = 0, outputfile = ''):
     
     site_mask = make_site_mask(ts.num_sites, percentmissing, 1234) #Makes sites from the number of sites in ts
@@ -83,9 +48,58 @@ def make_missing_vcf(ts, percentmissing = 0, outputfile = ''):
     
     return x
 
+
+pop_number = 20
+
+file = open("population.txt", "a")
+
+for x in range(0, pop_number):
+    file.write("tsk_"+ str(x) + "\t1\n")
+file.close()
+
+ts = msprime.sim_ancestry(20, random_seed=1, sequence_length=10000)
+
+#sequence length and site size have to be set to SAME number
+
+site_size = 10000 #sets number of sites
+
+rlg = np.random.default_rng(1234) #random letter generator 
+
+rchar = rlg.integers(low=0, high=4) #randomly choses number from 0 to 3 which represents genome
+
+difference_counter = range(site_size) 
+
+tables = ts.dump_tables() 
+
+for x in difference_counter: #Randomly assings reference genome for each site
+    
+    rchar = rlg.integers(low=0, high=4)
+        
+    if rchar == 0:
+        letter = "A"
+        
+    elif rchar == 1:
+        letter = "C"
+        
+    elif rchar == 2:
+        letter = "G"
+        
+    elif rchar == 3:
+        letter = "T"
+        
+    tables.sites.add_row(x, "A") 
+    
+ts = tables.tree_sequence()
+
+ts = msprime.sim_mutations(ts, rate=0.1, random_seed=1234) #Mutates the sites at random
+
 make_missing_vcf(ts,20,'my.vcf')
 
-os.system('cat my.vcf | grep "^#" > vcf_new.vcf')
+#os.system('cat my.vcf | grep "^#" > vcf_new.vcf')
 
-os.system('cat my.vcf | grep -v "^#" | awk -v s=1 \'{$2=$2+s; $3=$3+s; print}\' >> vcf_new.vcf')
-os.system('rm my.vcf')
+#os.system('cat my.vcf | grep -v "^#" | awk -v s=1 \'{$2=$2+s; $3=$3+s; print}\' >> vcf_new.vcf')
+#os.system('rm my.vcf')
+
+os.system('bgzip my.vcf')
+os.system('tabix -f -p vcf -0 my.vcf.gz')
+os.system('pixy --stats pi --vcf my.vcf.gz --populations population.txt --window_size 10000')
